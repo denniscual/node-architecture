1443cca36f541585e675d9275927e53f7fd42566
Dennis Cual <mamaylovepapay@outlook.com>
2018-03-15 21:56:25 +0800

    create server dir

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+function getName(){
+  
+}
+
+app.get(
+  '/',
+  (req, res) => res.send('Hello World!')
+)
+
+app
+  .listen(
+    3000,
+    () => console.log('App listening to port 3000!')
+  )
+
+exports.default = app
+
+/**
+ * Try to create simple application using Clean Architecture. The job of app is to create user using REST API then save the data to memory.
+ */
