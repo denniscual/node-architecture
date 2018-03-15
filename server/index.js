41b5e3cff5c1da3d8187f360648690d94fe3f211
Dennis Cual <mamaylovepapay@outlook.com>
2018-03-15 22:14:48 +0800

    add project structure for implementing Clean Architecture

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-function getName(){
-  
-}
-
 app.get(
   '/',
   (req, res) => res.send('Hello World!')
@@ -19,5 +15,15 @@ app
 exports.default = app
 
 /**
- * Try to create simple application using Clean Architecture. The job of app is to create user using REST API then save the data to memory.
+ * Create simple application using Clean Architecture. The job of app is to save user using REST API then save the data to memory.
+ *
+ * Rule of thumb:
+ * - Our business rules should work and testable even without interacting to external services. Business should not depend or doesn't know anything to outside layers.
+ * Make sure we follow the Dependencry Rule Principles - The flow of dependency towards Higher lever layers. Infrastructure -----> Interface Adapter -----> Use Case ---------> Entity
+ *
+ * TODO:
+ * - Create Entity (Critical Business rules)
+ * - Create use case / interactor  (Application Specific Business rules)
+ * - Create Interface adapter (Db gateways, controllers) - Use dependency injection for this. JS dont have Interface.
+ * - Add Infrastructure (Web frameworks, Persistence utility, View)
  */
