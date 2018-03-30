706448ff605def3d03bc0798de938ab14894f9ac
Dennis Cual <mamaylovepapay@outlook.com>
2018-03-30 21:43:15 +0800

    enhance the validation library. Extract all null data on the error messages array

--- src/services/users.js
+++ src/services/users.js
@@ -1,11 +1,19 @@
 const validator = require('validator')
-const { map, not } = require('ramda')
+const { compose, map, filter, not, isNil, isEmpty } = require('ramda')
 
 // Predicate function for validating if the data is not empty.
-const isNotEmpty = (data) => not(validator.isEmpty(data))
+const isNotEmpty = compose(not, isEmpty)
+
+// A function which evaluates the error data. If the data is not null or undefined, it returns true. Or else, return false.
+const isNotNil = compose(not, isNil)
+
+// Helper function for debugging data on function composition computation.
+const traced = (label) => (data) => {
+  console.log(`Label: ${label} -> Data: `, data)
+  return data
+}
 
 /**
-  * Test data: [isNotEmpty, 'This field should not be empty'], 'irish jane'
   * validate :: Array[Function, String] -> String -> String(Error Msg) || null
   *
   * A function which evaluates the data if it is valid or not based on the predicate function pass.
@@ -18,22 +26,43 @@ const validateInput = ([predicate, errMsg]) => (data) =>
 
 /**
   * validateInputs :: Array(a) -> Array(b)
+  *
   * Accepts an array of validation rules for each input and return an array of function that accepts the input data.
   * @param {Array} validationRules
   * @return {Array} Array of functions
-  * @function
   */
-const validateInputs = map(validateInput) // name: [[func, func]]
+const validateInputs = map(validateInput)
 
-// Execute the validators on a given input value.
-const runValidation = ({value, validators}) =>
-  map((pred) => pred(value))(validateInputs(validators)) // return [[func], [func]]
+/**
+ * extractNullMessages :: Array -> Array
+ *
+ * It extracts the null messages on the errors array
+ * @param {Array} errors Array of error messages.
+ * @return {Array}
+ */
+const extractNullMessages = filter(isNotNil) // return []
 
-// Validate the input based on the given validators. It return an object which assign an array of possible error messages on each field.
-const validate = map(runValidation)
+/**
+ * runValidationRules :: Object -> Array
+ *
+ * Execute the validators on a given input value then return an error messages. We will not include the null value on an array.
+ * @param {String} value The input data value.
+ * @param {Array} validators The validators for the given input.
+ * @return {Array} Array of error messages.
+ */
+const runValidationRules = ({value, validators}) =>
+  compose(extractNullMessages, map((validateInput) => validateInput(value)), validateInputs)(validators)
+
+/**
+ * validate :: Object(a) -> Object(b)
+ *
+ * Fork validation rules for the given inputs.
+ * @param {Object} data Data which the validate computation apply.
+ * @return {Object} Object with the same keys but the value are an array of error messages.
+ */
+const validate = compose(filter(isNotEmpty), map(runValidationRules))
 
 // Test the validation
-
 // test data
 const users = {
   username: {
@@ -41,6 +70,12 @@ const users = {
     validators: [
       [isNotEmpty, 'This field should not be empty'],
       [(data) => data.length > 5, 'Length of the field should be greater than 5']
+    ]
+  },
+  email: {
+    value: '',
+    validators: [
+      [validator.isEmail, 'Invalid email.']
     ]
   },
   firstName: {
@@ -52,7 +87,7 @@ const users = {
   }
 }
 
-// run the test. 
+// run the test.
 const result = validate(users)
 
 console.log(result)
